757df5d89c7b55ff9f223a7f6e57dea8354ce53d
josh warren <81135679+joshwrn@users.noreply.github.com>
2024-08-02 23:27:55 -0700

    deploy

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite';
-import ReactPlugin from '@vitejs/plugin-react';
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { defineConfig, loadEnv } from "vite"
+import ReactPlugin from "vite-preset-react"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [ReactPlugin({})],
-});
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  return defineConfig({
+    plugins: [
+      ReactPlugin({
+        // change this depending on whether you're importing from dist or src
+        injectReact: process.env.VITE_NODE_ENV === `production` ? true : false,
+      }),
+    ],
+    base: `/recoil-devtools/`,
+    server: {
+      fs: {
+        allow: [`..`],
+      },
+    },
+  })
+}
